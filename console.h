5191aa4087922adba572c14590036212e06a42ef
hoangpapa <truongnhathoang7@gmail.com>
2024-12-10 13:11:27 +0700

    test lan 2

--- console.h
+++ console.h
@@ -70,16 +70,6 @@ void boxDefault(int x, int y, int w, int h, int text_color) {
 	}
 }
 
-void boxIngredient() {
-	boxDefault(5, 5, 100, 20, 15);//box lon
-	boxDefault(30, 6, 70, 17, 15);//box lon ben phai
-	boxDefault(7, 6, 20, 14, 15);//box lon ben trai
-	//boxColor(9, 8, 16, 2, 15,1,"Dang ky LTC");//box nho 1 ben trai
-	//boxColor(9, 11, 16, 2, 15,1,"In DSSVDDK");//box nho 2 ben trai
-	//boxColor(9, 14, 16, 2, 15,1,"Danh sach LTC");//box nho 3 ben trai
-	//boxColor(9, 17, 16, 2, 15,1,"Thoat");//box nho 4 ben trai
-}
-
 
 
 
